469471806b2bb33005aeb4331069bd8af180948d
Francesco <perrinofrancesco93@gmail.com>
2023-12-24 03:18:19 +0100

    Project creation.

---
+++ src/secrets.h
@@ -0,0 +1,11 @@
+// Replace with your network credentials
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+const String chatId = "xxxxxxxx";
+
+// Initialize Telegram BOT
+const String BOTtoken = "xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
